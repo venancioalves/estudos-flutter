9cbcc667d08189675ec6ce9b5756f124d0267e9e
venancioalves <venaciobsn@gmail.com>
2023-10-06 21:23:34 -0300

    Update main.dart

--- primeiroprojeto/lib/main.dart
+++ primeiroprojeto/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Clica ai macho para adicionar ao contador: ',
             ),
             Text(
               '$_counter',

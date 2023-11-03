f294c69fa26d970bd128e620c4d58cc9119cdee3
venancioalves <venaciobsn@gmail.com>
2023-11-03 14:09:53 -0300

    Formatação de textos
    Aprendi a formatar textos 😁

--- curso-desenvolvimento/secao-10/frases_do_dia/lib/main.dart
+++ curso-desenvolvimento/secao-10/frases_do_dia/lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Frases do dia",
-    home: Row(
-      children: [Text("T01"), Text("T02"), Text("T03")],
-    ),
-  ));
+      title: "Frases do dia",
+      home: Container(
+        color: Colors.red,
+        child: Column(
+          children: [
+            Text(
+              "Bem vindo ao Grupo Atan! Somos o maior distribuidor de eletrodomesticos do Nordeste!",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w800),
+            ),
+          ],
+        ),
+      )));
 }

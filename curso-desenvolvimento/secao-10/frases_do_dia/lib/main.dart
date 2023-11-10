567557d4d38214eb8a6a4cd485e11db233b8246f
venancioalves <venaciobsn@gmail.com>
2023-11-10 10:21:14 -0300

    aprendendo Espaçamentos

--- curso-desenvolvimento/secao-10/frases_do_dia/lib/main.dart
+++ curso-desenvolvimento/secao-10/frases_do_dia/lib/main.dart
@@ -4,15 +4,20 @@ void main() {
   runApp(MaterialApp(
       title: "Frases do dia",
       home: Container(
-        color: Colors.white,
+        //color: Colors.white,
+        padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
+        margin: EdgeInsets.all(30),
+        decoration: BoxDecoration(
+            border: Border.all(
+                width: 5, color: const Color.fromARGB(255, 255, 0, 0))),
         child: Column(
           children: [
-            FloatingActionButton(
-              onPressed: () {
-                print("Apertou o botão!");
-              },
-              child: Text("Botão"),
-            )
+            Text("T1"),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Text("T2"),
+            ),
+            Text("T3"),
           ],
         ),
       )));

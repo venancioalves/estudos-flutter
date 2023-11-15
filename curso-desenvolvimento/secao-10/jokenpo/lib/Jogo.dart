e3000ea41a5c4d1171ecaa1a60a819769c9a3f93
venancioalves <venaciobsn@gmail.com>
2023-11-14 21:18:22 -0300

    Feat: concluindo o kokenpo

--- curso-desenvolvimento/secao-10/jokenpo/lib/Jogo.dart
+++ curso-desenvolvimento/secao-10/jokenpo/lib/Jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Jogo extends StatefulWidget {
@@ -8,6 +10,61 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+  var _imagemApp = AssetImage("../assets/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo:";
+
+  void _opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    print("Escolha do app" + escolhaApp);
+    print("Escolha do usuario" + escolhaUsuario);
+
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          this._imagemApp = AssetImage("../assets/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          this._imagemApp = AssetImage("../assets/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imagemApp = AssetImage("../assets/tesoura.png");
+        });
+        break;
+    }
+
+    //validacao do ganhador
+
+    if (
+        //usuario ganhador
+        (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+            (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+            (escolhaUsuario == "papel" && escolhaApp == "pedra")) {
+      setState(() {
+        this._mensagem = "Você ganhou!";
+      });
+    } else if (
+        //usuario ganhador
+        (escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+            (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+            (escolhaApp == "papel" && escolhaUsuario == "pedra")) {
+      setState(() {
+        this._mensagem = "O App ganhou hehehe!";
+      });
+    } else {
+      setState(() {
+        this._mensagem = "Empatamos kkkkk";
+      });
+    }
+    ;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,19 +87,25 @@ class _JogoState extends State<Jogo> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              print("Pressionou o botão!");
-            },
-            child: Image.asset(
-              '../assets/padrao.png',
-              height: 100,
-            ),
+          // GestureDetector(
+          //   onTap: () {
+          //     print("Pressionou o botão!");
+          //   },
+          //   child: Image.asset(
+          //     '../assets/padrao.png',
+          //     height: 100,
+          //   ),
+          // ),
+
+          Image(
+            image: this._imagemApp,
+            height: 150,
           ),
+
           Padding(
             padding: EdgeInsets.only(top: 20, bottom: 10),
             child: Text(
-              "Escolha uma opção abaixo:",
+              this._mensagem,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
@@ -50,17 +113,26 @@ class _JogoState extends State<Jogo> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset(
-                '../assets/pedra.png',
-                height: 100,
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset(
+                  '../assets/pedra.png',
+                  height: 100,
+                ),
               ),
-              Image.asset(
-                '../assets/papel.png',
-                height: 100,
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel"),
+                child: Image.asset(
+                  '../assets/papel.png',
+                  height: 100,
+                ),
               ),
-              Image.asset(
-                '../assets/tesoura.png',
-                height: 100,
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"),
+                child: Image.asset(
+                  '../assets/tesoura.png',
+                  height: 100,
+                ),
               ),
             ],
           )

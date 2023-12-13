81e1b1e1e6a72207ea74432c39deaadcb37f753a
venancioalves <venaciobsn@gmail.com>
2023-12-13 09:44:35 -0300

    FLUTTER NV1 - [07] PageView Widget - Parte 2

--- FLUTTER-NV1/aula07/lib/pages/home_page.dart
+++ FLUTTER-NV1/aula07/lib/pages/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'pageViews/one_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   PageController _pageController = PageController();
+
+  int indexBottomNavigationBar = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,35 +22,7 @@ class HomePage extends StatelessWidget {
       body: PageView(
         controller: _pageController,
         children: [
-          Column(children: [
-            Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  'Olá Mundo!',
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  color: Colors.red,
-                  height: 100,
-                  width: 150,
-                ),
-                Container(
-                  color: Color.fromARGB(255, 226, 5, 215),
-                  height: 100,
-                  width: 150,
-                ),
-              ],
-            )
-          ]),
+          OnePage(),
           Container(
             color: Colors.red,
           ),
@@ -55,16 +35,24 @@ class HomePage extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-          onTap: (int page) => _pageController.animateToPage(page,
-              duration: Duration(milliseconds: 300), curve: Curves.ease),
+
+          //faz o contador das paginas
+          currentIndex: indexBottomNavigationBar,
+          onTap: (int page) {
+            setState(() {
+              indexBottomNavigationBar = page;
+            });
+            _pageController.animateToPage(page,
+                duration: Duration(milliseconds: 300), curve: Curves.ease);
+          },
           items: [
             BottomNavigationBarItem(
                 icon: Icon(Icons.home),
-                label: 'Home',
+                label: 'Inicio',
                 backgroundColor: Colors.grey),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
-            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'list'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Home')
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Serviços'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil')
           ]),
     );
   }

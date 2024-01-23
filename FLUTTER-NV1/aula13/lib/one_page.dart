208e676ef314d01276ad7037bffa7e89388ceb3d
venancioalves <venaciobsn@gmail.com>
2024-01-23 18:23:11 -0300

    Ajustando para consumir a API do video

--- FLUTTER-NV1/aula13/lib/one_page.dart
+++ FLUTTER-NV1/aula13/lib/one_page.dart
@@ -22,8 +22,8 @@ class _OnePageState extends State<OnePage> {
     var client = http.Client();
     try {
       var response = await client.get(
-          Uri.parse('http://192.168.1.248:8080/user'),
-          headers: {"Content-Type": "application/json"});
+        Uri.parse('http://jsonplaceholder.typicode.com/posts'),
+      );
       var decodedResponse = jsonDecode(response.body) as List;
       posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
     } finally {
@@ -46,7 +46,7 @@ class _OnePageState extends State<OnePage> {
                     shrinkWrap: true,
                     itemCount: value.length,
                     itemBuilder: (_, idx) => ListTile(
-                          title: Text(value[idx].name),
+                          title: Text(value[idx].title),
                         )),
               ),
               SizedBox(
@@ -66,43 +66,18 @@ class _OnePageState extends State<OnePage> {
 }
 
 class Post {
-  int id;
-  String name;
-  String login;
-  String password;
-  String email;
-  String phone;
-  String code_sales_person;
-  String type_sales;
-  int id_profile;
-  String tbl_price;
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
 
-  Post(
-      {required this.id,
-      required this.name,
-      required this.login,
-      required this.password,
-      required this.email,
-      required this.phone,
-      required this.code_sales_person,
-      required this.type_sales,
-      required this.id_profile,
-      required this.tbl_price});
+  Post(this.userId, this.id, this.title, this.body);
 
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-        id: json['id'],
-        name: json['name'],
-        login: json['login'],
-        password: json['password'],
-        email: json['email'],
-        phone: json['phone'],
-        code_sales_person: json['code_sales_person'],
-        type_sales: json['type_sales'],
-        id_profile: json['id_profile'],
-        tbl_price: json['tbl_price']);
+  factory Post.fromJson(Map json) {
+    return Post(json['userId'], json['id'], json['title'], json['body']);
   }
 }
+
   
 
 

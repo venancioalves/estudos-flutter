7b8aad77b028113cd97ba0b9251168293ac315d5
venancioalves <venaciobsn@gmail.com>
2024-01-25 18:33:38 -0300

    Create login_controller.dart

---
+++ FLUTTER-NV1/projeto01/lib/controllers/login_controller.dart
@@ -0,0 +1,11 @@
+class LoginController {
+  String? _login;
+  setLogin(String value) => _login = value;
+
+  String? _pass;
+  setPass(String value) => _pass = value;
+
+  Future<bool> auth() async {
+    return false;
+  }
+}
